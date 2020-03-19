ea23c895756e766991f448184a8e5fddb4527698
Shaunak <shaunak.badani@research.iiit.ac.in>
2020-03-19 13:26:23 +0530

    Cleaned up directory, changed filenames to match assignment requirements

--- Assignment 4/20171004_1.erl
+++ Assignment 4/20171004_1.erl
@@ -1,8 +1,12 @@
--module(q1).
--export([main/1, spawnProcesses/4, send_and_receive_token/3, send_and_receive_token/2, spawnRemainingProcesses / 5, print_to_file/3]).
+-module('20171004_1').
+-compile(export_all).
 
 print_to_file(Outp_filename, StrVal,  Arr) ->
     file:write_file(Outp_filename, io_lib:fwrite(StrVal, Arr), [append]).
+
+% The root process first accepts the PID of the next process.
+% It then sends out the token to the next process, and 
+% Waits to receive the token from the N-1th process.
 
 send_and_receive_token(0, Token, Outp_filename) ->
     receive
@@ -23,16 +27,27 @@ send_and_receive_token(Rank, Outp_filename) ->
             end
     end.
 
+% Params : 
+% Rank : Rank of the process spawned. Gives identity to process apart from PID.
+% N : No of processes to be spawned.
+% Prev_ID : The Process ID which is to be sent to the process spawned in current iteration.
+% Root : Process ID of the first process
+% Outp_filename : Self Explanatory
+
 spawnRemainingProcesses(Rank, N, Prev_ID, Root, Outp_filename) when Rank < N ->
-    New_ID = spawn(q1, send_and_receive_token, [Rank, Outp_filename]),
+    New_ID = spawn(?MODULE, send_and_receive_token, [Rank, Outp_filename]),
     Prev_ID ! {sender_id, New_ID},
     spawnRemainingProcesses(Rank + 1, N, New_ID, Root, Outp_filename);
 spawnRemainingProcesses(_, _, Prev_ID, Root, _) ->
     Prev_ID ! {sender_id, Root}.
 
+% Spawn processes has a special definition for the 0th rank as it  
+% must accept the token. The token must not be passed to any other 
+% process when it is spawned. It will receive the token from its
+% predecessor.
 
 spawnProcesses(0, N, Token, Outp_filename) ->
-    Root = spawn(q1, send_and_receive_token, [0, Token, Outp_filename]),
+    Root = spawn(?MODULE, send_and_receive_token, [0, Token, Outp_filename]),
     spawnRemainingProcesses(1, N, Root, Root, Outp_filename).
 
 
--- Assignment 4/20171004_2.erl
+++ Assignment 4/20171004_2.erl
@@ -1,8 +1,14 @@
--module(q2).
--export([main/1, print_to_file/3, print_list/2, merge / 2, mergeSort / 3, receive_all/2, receive_all/4, spawnProcesses/7, sort_and_send/1, receive_sublist/1, spawn_and_send/4, collect_array_from_file/2]).
+-module('20171004_2').
+-import(lists,[min/1]).
+% -export([main/1, print_to_file/3, print_list/2, merge / 2, mergeSort / 3, receive_all/2, receive_all/4, split_into_sublists/7, sort_and_send/1, receive_sublist/1, spawn_and_send/4, collect_array_from_file/2]).
+-compile(export_all).
+
+% HELPER FUNCTION PRINT_TO_FILE
 
 print_to_file(Outp_filename, StrVal,  Arr) ->
     file:write_file(Outp_filename, io_lib:fwrite(StrVal, Arr), [append]).
+
+% HELPER FUNCTION TO PRINT LIST
 
 print_list([ ], Outp_filename) ->
     print_to_file(Outp_filename, "~n", []);
@@ -10,7 +16,9 @@ print_list([F | Rest], Outp_filename) ->
     print_to_file(Outp_filename, "~w ", [F]),
     print_list(Rest, Outp_filename).
 
-% Start mergesort code
+% END HELPER FUNCTIONS
+
+% MERGESORT
 
 merge([], Back) ->
     Back;
@@ -30,16 +38,16 @@ mergeSort(X, L, _) ->
     P = lists:nth(L, X),
     [P].
 
-% End mergesort code
+% END MERGESORT
 
-% Process wise sublist sorting
+% PROCESS WISE SUBLIST SORTING
 
 sort_and_send(SubList) ->
     L = length(SubList),
     Sorted = mergeSort(SubList, 1, L),
     Sorted.
 
-% End Process wise sublist sorting
+% END PROCESS WISE SUBLIST SORTING
 
 receive_sublist(1) ->
     receive
@@ -71,44 +79,54 @@ receive_all(No_of_processes, Outp_filename) ->
 % End collection
 
 spawn_and_send(SubList, 1, No_of_processes, Outp_filename) ->
-    register(root_process, spawn(q2, receive_all, [No_of_processes, Outp_filename])),
-    Root_Pid = spawn(q2, receive_sublist, [1]),
+    register(root_process, spawn(?MODULE, receive_all, [No_of_processes, Outp_filename])),
+    Root_Pid = spawn(?MODULE, receive_sublist, [1]),
     Root_Pid ! {sublist, SubList};
 spawn_and_send(SubList, Rank, _, _) ->
-    PID = spawn(q2, receive_sublist, [Rank]),
+    PID = spawn(?MODULE, receive_sublist, [Rank]),
     PID ! {sublist, SubList}.
 
-% Spawn processes 
+% SPLIT INTO SUBLISTS
 
-spawnProcesses(Current_Rank, Array, [], Numbers_Per_Proc, Len, No_of_processes, Outp_filename) ->
+% This function splits the list given into sublists as 
+% per the requirements.
+% Params : 
+% Current_Rank : Rank of the process in the current iteration
+% Array : Original Array that is to be split. Array is "popped" from the front at every iteration.
+% Numbers_Per_Proc : Approximate length of each sublist.
+% Len : Length of the current Sublist
+% No_of_processes, Outp_filename : Self explanatory
+
+split_into_sublists(Current_Rank, Array, [], Numbers_Per_Proc, Len, No_of_processes, Outp_filename) ->
     [Current | Rest ] = Array,
     Arr_Acc = [Current],
     Len_dup = Len + 1,
-    spawnProcesses(Current_Rank, Rest, Arr_Acc, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename);
-spawnProcesses(Current_Rank, [], Arr_Acc, __, _, No_of_processes, Outp_filename) ->
+    split_into_sublists(Current_Rank, Rest, Arr_Acc, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename);
+split_into_sublists(Current_Rank, [], Arr_Acc, __, _, No_of_processes, Outp_filename) ->
     spawn_and_send(Arr_Acc, Current_Rank, No_of_processes, Outp_filename);
-spawnProcesses(Current_Rank, Array, Arr_Acc, Numbers_Per_Proc, Len, No_of_processes, Outp_filename) ->
+split_into_sublists(Current_Rank, Array, Arr_Acc, Numbers_Per_Proc, Len, No_of_processes, Outp_filename) ->
     if 
         Current_Rank == No_of_processes ->
             [Current | Rest] = Array,
             Arr_A = lists:append(Arr_Acc, [Current]),
             Len_dup = Len + 1,
-            spawnProcesses(Current_Rank, Rest, Arr_A, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename);
+            split_into_sublists(Current_Rank, Rest, Arr_A, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename);
         Len == Numbers_Per_Proc ->
             spawn_and_send(Arr_Acc, Current_Rank, No_of_processes, Outp_filename),
             Arr_B = [],
             Current_Rank_dup = Current_Rank + 1,
             Len_dup_1 = 0,  
-            spawnProcesses(Current_Rank_dup, Array, Arr_B, Numbers_Per_Proc, Len_dup_1, No_of_processes, Outp_filename);
+            split_into_sublists(Current_Rank_dup, Array, Arr_B, Numbers_Per_Proc, Len_dup_1, No_of_processes, Outp_filename);
         true ->
             [Current | Rest] = Array,
             Arr_A = lists:append(Arr_Acc, [Current]),
             Len_dup = Len + 1,
-            spawnProcesses(Current_Rank, Rest, Arr_A, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename)
+            split_into_sublists(Current_Rank, Rest, Arr_A, Numbers_Per_Proc, Len_dup, No_of_processes, Outp_filename)
     end.
 
-% End spawning of processes
+% END SPLITTING INTO SUBLISTS
 
+% HELPER FUNCTION FOR COLLECTING ARRAY FROM FILE
 collect_array_from_file(Filename, _) ->
     {ok, FileBuffer} = file:open(Filename, [read]),
     {ok, Line} = file:read_line(FileBuffer),
@@ -117,10 +135,9 @@ collect_array_from_file(Filename, _) ->
     X.
 
 main([Inp_filename | Outp_filename]) ->
-    file:delete(Outp_filename),
-    io:format("~w ~n", [Outp_filename]),
+    file:delete(Outp_filename), % Deletes file if exists
     X = collect_array_from_file(Inp_filename, Outp_filename),
     L = length(X),
-    No_of_processes = 8,
+    No_of_processes = min([L, 8]),
     Numbers_Per_Proc = erlang:list_to_integer(erlang:float_to_list(L / No_of_processes ,[{decimals,0}])),
-    spawnProcesses(1, X, [], Numbers_Per_Proc, 0, No_of_processes, Outp_filename).
+    split_into_sublists(1, X, [], Numbers_Per_Proc, 0, No_of_processes, Outp_filename).
